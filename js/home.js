370c3868e58c20c20356e1af317361b3116a98d0
stephanyas <stephanyass@hotmail.com>
2021-02-18 22:25:21 -0300

    Ajustes do botão veja mais e inclusão dos data-mask no checkout

--- js/home.js
+++ js/home.js
@@ -1,3 +1,3 @@
-$('.novidades button').click(function(){
-    $('.novidades').toggleClass('painel-aberto');
+$('.painel button').click(function(){
+    $(this).parent().toggleClass('painel-aberto');
 });
