4b763614fef1e0de6a550ab2e75aef9a00b143bf
Stephany <stephany@pixemotion.com.br>
2022-04-27 18:42:35 -0300

    Ajustes finais
    Finalizando o projeto

--- js/banner.js
+++ js/banner.js
@@ -1,19 +1,22 @@
 var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
 var bannerAtual = 0;
-function trocarBanner(){
-    bannerAtual = (bannerAtual+1)%2;
-    document.querySelector('.banner-destaque img').src = banners[bannerAtual];    
-}
-setInterval(trocarBanner, 4000);
 
-var timer = setInterval(trocarBanner, 4000);
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.banner-destaque img').src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
+
+var timer = setInterval(trocaBanner, 4000);
 var controle = document.querySelector('.pause');
-controle.onclik = function(){
-    if(controle.className == 'pause'){
+
+controle.onclick = function() {
+    if (controle.className == 'pause') {
         clearInterval(timer);
         controle.className = 'play';
-    }else{
-        timer = setInterval(trocarBanner, 4000);
+    } else {
+        timer = setInterval(trocaBanner, 4000);
         controle.className = 'pause';
     }
     return false;
